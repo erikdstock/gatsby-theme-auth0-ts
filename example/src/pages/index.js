564835cf1f92150156737c2409bc0b42724a6561
Erik Stockmeier <erik.d.stock@gmail.com>
2019-10-13 19:28:31 -0400

    add login credentials to example app

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -5,6 +5,12 @@ export default () => {
   return (
     <Layout>
       <h2>Home</h2>
+      <pre>
+        <h3>Auth0 Login Credentials:</h3>
+        User: example@test.com
+        <br />
+        Password: Password123
+      </pre>
     </Layout>
   )
 }
